ddc6e55a033eb916c41cfb7b5ed991f38723c471
UltralyticsAssistant <web@ultralytics.com>
2026-01-25 04:28:51 +0000

    Auto-format by https://ultralytics.com/actions

--- zzz/showpt.py
+++ zzz/showpt.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
 # 自动下载 + 加载 yolo26n.pt（如果本地没有）
-model = YOLO('yolo12n.pt')
+model = YOLO("yolo12n.pt")
 
 # 打印模型信息
-#print(model)
+# print(model)
 
 # 查看任务类型（检测/分割/姿态等）
 print(f"Model task: {model.task}")
@@ -12,6 +12,6 @@ print(f"Model task: {model.task}")
 # 查看配置
 print(f"Model config: {model.cfg}")
 
-#查看参数
+# 查看参数
 total = sum(p.numel() for p in model.parameters())
-print(f"Params: {total / 1e6:.2f}M") #yolo26n=2.57m
+print(f"Params: {total / 1e6:.2f}M")  # yolo26n=2.57m
